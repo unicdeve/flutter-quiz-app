24faf97b24cccde5c0ff6ed4862787b047981d53
Taiwo <uniqueomokenny@gmail.com>
2021-01-14 12:50:00 +0100

    Basic layout with Scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,40 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer selected');
+  }
+
+  var questions = [
+    "What's your favorite color?",
+    "What's your favorite animal",
+  ];
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello taiwo'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My second app'),
+        ),
+        body: Column(
+          children: [
+            Text('The question!'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
